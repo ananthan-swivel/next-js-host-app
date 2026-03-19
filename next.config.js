3d97821f5be97187cd14f2a51d3cc4fd2f8168b2
Ananthan Gnanamoorthy <ananthan@swivelgroup.com.au>
2026-03-19 12:10:06 +0530

    feat: add LogSync component for displaying and managing log entries

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		NEXT_PUBLIC_REMOTE_APP_URL: process.env.NEXT_PUBLIC_REMOTE_APP_URL,
+		REMOTE_APP_SECRET: process.env.REMOTE_APP_SECRET,
+	},
+	assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
+	async rewrites() {
+		return [
+			{
+				source: '/remote/:path*',
+				destination: `${process.env.NEXT_PUBLIC_REMOTE_APP_URL}/:path*`,
+			},
+			{
+				source: '/remote-static/:path*',
+				destination: `${process.env.NEXT_PUBLIC_REMOTE_APP_URL}/remote-static/:path*`,
+			},
+		];
+	},
+}
 
 module.exports = nextConfig
